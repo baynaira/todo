72bc2fac1c5a54aa90d05555a6beab157a1f81a3
bay naira <me@gmail.com>
2024-09-24 06:40:43 -0700

    fix ererr

--- app/models.py
+++ app/models.py
@@ -1,13 +1,10 @@
 from django.db import models
-
-# Create your models here.
 from django.utils import timezone
- 
- 
+
 class Todo(models.Model):
     title = models.CharField(max_length=100)
     details = models.TextField()
     date = models.DateTimeField(default=timezone.now)
- 
+
     def __str__(self):
-        return self.title
+        return self.title  # Return the title directly as a string
